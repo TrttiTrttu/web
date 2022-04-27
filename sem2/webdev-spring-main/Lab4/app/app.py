6e9e145b5211a41f3977c4271945a45e5ea81d7e
TrttiTrttu <trtti.trttu@gmail.com>
2022-04-27 15:16:09 +0300

    lab 4 1st lesson

--- sem2/webdev-spring-main/Lab4/app/app.py
+++ sem2/webdev-spring-main/Lab4/app/app.py
@@ -1,77 +1,71 @@
-
-from flask import Flask, make_response, render_template, request, session, redirect, url_for, flash
+from cmath import log
+from flask import Flask, redirect, render_template, request, session, url_for, flash
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required
 from mysql_db import MySQL
+
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.login_message = 'Для доступа к данной странице необходимо пройти процедуру аутентификации.'
+login_manager.login_message_category = 'warning'
 
 app = Flask(__name__)
 application = app
 
-login_manager = LoginManager()
+app.config.from_pyfile('config.py')
 
 login_manager.init_app(app)
-login_manager.login_view = 'login'
-login_manager.login_message = 'авторизуйтесь'
-login_manager.login_message_category = 'warning'
-
 
 mysql = MySQL(app)
+
 
 class User(UserMixin):
     def __init__(self, user_id, login):
         super().__init__()
         self.id = user_id
         self.login = login
- 
+
 
 @login_manager.user_loader
-def load_user(user_id):
+def loadUser(user_id):
     with mysql.connection.cursor(named_tuple=True) as cursor:
         cursor.execute('SELECT * FROM users WHERE id=%s;', (user_id,))
         db_user = cursor.fetchone()
     if db_user:
-        return User(user_id=db_user.id, login=db_user.login)        
+        return User(user_id=db_user.id, login=db_user.login)
     return None
 
-
-
-app.config.from_pyfile('config.py')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/visits')
-def visits():
-    if session.get('visits_count') is None:
-        session['visits_count'] = 1
-    else:
-        session['visits_count'] += 1
-    return render_template('visits.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         login_ = request.form.get('login')
         password_ = request.form.get('password')
-        remember_me = request.form.get('remember_me') == 'on'
+        remember_me_ = request.form.get('remember_me') == 'on'
+
         with mysql.connection.cursor(named_tuple=True) as cursor:
-            cursor.execute('SELECT * FROM users WHERE login=%s AND password_hash=SHA2(%s, 256);', (login_, password_))
+            cursor.execute(
+                'SELECT * FROM users WHERE login=%s and password_hash=SHA2(%s, 256);', (login_, password_))
             db_user = cursor.fetchone()
-            if db_user:
-                login_user(User(user_id=db_user.id, login=db_user.login),
-                    remember=remember_me)
-                flash('Классный вход', 'success')
-                next_ = request.args.get('next')
-                return redirect(next_ or url_for('index'))
-        flash('Плохо, что-то плохо', 'danger')
+
+        if db_user:
+            login_user(
+                User(user_id=db_user.id, login=db_user.login), remember=remember_me_)
+
+            flash('Вы успешно прошли процедуру аутентификации.', 'success')
+            next_ = request.args.get('next')
+            return redirect(next_ or url_for('index'))
+
+        flash('Введены неверные логин и/или пароль.', 'danger')
+
     return render_template('login.html')
+
 
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
-
-@app.route('/secret_page')
-@login_required
-def secret_page():
-    return render_template('secret_page.html')
