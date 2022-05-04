74e102c602b29a0ea3af09853ce0a2e09590f000
TrttiTrttu <trtti.trttu@gmail.com>
2022-05-04 19:11:31 +0300

    lab5, fixed the kosyaks

---
+++ sem2/webdev-spring-main/Lab5/app/config.py
@@ -0,0 +1,6 @@
+SECRET_KEY = '6415c23e7c2d000e95d4b157c0522e33fde56fae2f5364ced196966e49a25a6b'
+MYSQL_USER = 'std_1695_lab4'
+MYSQL_DATABASE = 'std_1695_lab4'
+MYSQL_HOST = 'std-mysql.ist.mospolytech.ru'
+MYSQL_PASSWORD = 'zxczxczxc'
+ADMIN_ROLE_ID = 1
